45e01be00bdd202821165e2e1ca19e9c6a441f49
Christoph Höger <christoph.hoeger@tu-berlin.de>
2015-01-03 16:51:59 +0100

    Add long missing syntax fragments file
    Oops .. Should have used CI to catch that
    
    Signed-off-by: Christoph Höger <christoph.hoeger@tu-berlin.de>

---
+++ src/syntax_fragments.ml
@@ -0,0 +1,41 @@
+(*
+ * Copyright (c) 2014, TU Berlin
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the TU Berlin nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL TU Berlin BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *)
+
+(** useful fragments of Modelica syntax *)
+
+open Syntax
+
+let no_comment = { annotated_elem = None ; annotation = None }
+
+let uncommented a = { commented = a ; comment = no_comment }
+
+let no_modification = { types = [] ; components = [] ; modifications = [] }
+
+let no_def_options = { final = false ; replaceable = false ; scope = Local ; visibility = Public }
+                        
+let empty_def  = { def_name ="" ; def_type = TIde ""; def_options = no_def_options ; def_constraint = None ; def_rhs = None ; def_if = None }
