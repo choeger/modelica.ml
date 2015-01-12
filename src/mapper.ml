65b28f915184cc80887ea9263bb196b7610e606e
Christoph Höger <christoph.hoeger@tu-berlin.de>
2015-01-12 11:05:36 +0100

    map modifications
    Signed-off-by: Christoph Höger <christoph.hoeger@tu-berlin.de>

--- src/mapper.ml
+++ src/mapper.ml
@@ -78,6 +78,13 @@ type mapper = {
   equation_desc : mapper -> equation_desc -> equation_desc;
   equation : mapper -> equation -> equation ;
 
+  modification : mapper -> modification -> modification;
+  type_redeclaration : mapper -> type_redeclaration -> type_redeclaration ;
+  component_redeclaration : mapper -> component_redeclaration -> component_redeclaration ;
+  component_modification : mapper -> component_modification -> component_modification ;
+  component_modification_struct : mapper -> component_modification_struct -> component_modification_struct ;
+  modification_value : mapper -> modification_value -> modification_value ;
+  
   name : mapper -> name -> name ;
   identifier : mapper -> string -> string;
   comment_str : mapper -> string -> string ;
@@ -104,19 +111,19 @@ let id this x = x
                                  
 (** Map for type-definitions *)                
 module TD = struct
-  let map_tds this sub { td_name ; sort ; type_exp ; cns ; type_options } =
+  let map_tds sub this { td_name ; sort ; type_exp ; cns ; type_options } =
     let type_options = this.typedef_options this type_options in
     let type_exp = sub this type_exp in
     let cns = Option.map (this.constraint_ this) cns in
     { td_name ; sort ; type_exp ; cns ; type_options }
 
   let map_desc this = function
-      Short tds -> Short (map_tds this this.texp tds)
-    | Composition tds -> Composition (map_tds this this.composition tds)
-    | Extension tds -> Extension (map_tds this this.extension tds)
-    | Enumeration tds -> Enumeration (map_tds this (map_list this.enum_literal) tds)
-    | OpenEnumeration tds -> OpenEnumeration (map_tds this id tds)       
-    | DerSpec tds -> DerSpec (map_tds this this.der_spec tds)
+      Short tds -> Short (map_tds this.texp this tds)
+    | Composition tds -> Composition (map_tds this.composition this tds)
+    | Extension tds -> Extension (map_tds this.extension this tds)
+    | Enumeration tds -> Enumeration (map_tds (map_list this.enum_literal) this tds)
+    | OpenEnumeration tds -> OpenEnumeration (map_tds id this tds)       
+    | DerSpec tds -> DerSpec (map_tds this.der_spec this tds)
                                                          
   let map = map_commented map_desc
 
@@ -153,7 +160,46 @@ module Comment = struct
   let map this { annotated_elem ; annotation } = { annotated_elem = (map_option &&& map_located) this.comment_str this annotated_elem ;
                                                    annotation = map_option this.annotation this annotation }
 end
-                                  
+
+module TRD = struct
+  (** Map type redeclarations *)
+  let map this { redecl_each ; redecl_type } =
+    { redecl_each ; redecl_type = map_commented (TD.map_tds this.texp) this redecl_type }
+end
+
+module CRD = struct
+  (** Map component redeclarations *)
+  let map this { each ; def } =
+    { each ; def = this.def this def }
+end
+
+module CMOD = struct
+  let map this = (map_commented this.component_modification_struct) this
+end
+               
+module CMOD_Struct = struct
+  (** Map component modifications *)
+  let map this { mod_each ; mod_final ; mod_name ;
+                 mod_value ; } = {mod_each ; mod_final ;
+                                  mod_name = this.name this mod_name ;                                  
+                                  mod_value = map_option this.modification_value this mod_value }
+end
+
+module CMOD_Value = struct
+  let map this = function
+      Nested modification -> Nested (this.modification this modification)
+    | Rebind exp -> Rebind (this.exp this exp)
+    | NestedRebind { nested ; new_value } -> NestedRebind { nested = this.modification this nested ;
+                                                            new_value = this.exp this new_value
+                                                          }
+end
+                       
+module Modification = struct
+  let map this {types ; components; modifications } = { types = map_list this.type_redeclaration this types ;
+                                                        components = map_list this.component_redeclaration this components ;
+                                                        modifications = map_list this.component_modification this modifications }
+end
+                   
 let default_mapper = {
   unit_ = Unit.map ;
   within = id;
@@ -202,5 +248,12 @@ let default_mapper = {
   identifier = id;
   comment_str = id;
   location = id;
+
+  modification = Modification.map;
+  type_redeclaration = TRD.map;
+  component_redeclaration = CRD.map;
+  component_modification = CMOD.map;
+  component_modification_struct = CMOD_Struct.map;
+  modification_value = CMOD_Value.map;
 };
 
